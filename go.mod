6805146811e8624d49600e672b7622d7a0578011
WhoMeNope <whomenope@outlook.com>
2019-02-17 13:55:10 +0100

    authenticate twitter

--- go.mod
+++ go.mod
@@ -2,8 +2,13 @@ module github.com/tekwrks/publish
 
 require (
 	github.com/Netflix/go-env v0.0.0-20180529183433-1e80ef5003ef
+	github.com/cenkalti/backoff v2.1.1+incompatible // indirect
+	github.com/dghubble/go-twitter v0.0.0-20190108053744-7fd79e2bcc65
+	github.com/dghubble/oauth1 v0.5.0
+	github.com/dghubble/sling v1.2.0 // indirect
 	github.com/go-stack/stack v1.8.0 // indirect
 	github.com/golang/snappy v0.0.0-20180518054509-2e65f85255db // indirect
+	github.com/google/go-querystring v1.0.0 // indirect
 	github.com/mongodb/mongo-go-driver v0.3.0
 	github.com/qiangxue/fasthttp-routing v0.0.0-20160225050629-6ccdc2a18d87
 	github.com/valyala/fasthttp v1.1.0
--- twitter.go
+++ twitter.go
@@ -5,7 +5,11 @@ import (
 	"log"
 
 	"github.com/mongodb/mongo-go-driver/bson"
+	"github.com/mongodb/mongo-go-driver/bson/primitive"
 	"github.com/qiangxue/fasthttp-routing"
+
+	"github.com/dghubble/go-twitter/twitter"
+	"github.com/dghubble/oauth1"
 )
 
 func (sc *serverContext) handleTwitter(c *routing.Context) error {
@@ -23,22 +27,46 @@ func (sc *serverContext) handleTwitter(c *routing.Context) error {
 
 	log.Println("got userID :", userID, "and postID :", postID)
 
+	// get post
+
 	// get user secret and token
+	userOID, err := primitive.ObjectIDFromHex(userID)
+	if err != nil {
+		log.Println("could not convert", userID, "into ObjectID")
+		return routing.NewHTTPError(400, "Invalid user.")
+	}
+
 	var user struct {
-		twitter struct {
-			token  string
-			secret string
+		Twitter struct {
+			Token  string
+			Secret string
 		}
 	}
-	filter := bson.M{"_id": userID}
-	err := sc.users.FindOne(context.Background(), filter).Decode(&user)
+	filter := bson.M{"_id": userOID}
+	err = sc.users.FindOne(context.Background(), filter).Decode(&user)
 	if err != nil {
-		log.Fatal(err)
+		log.Println("could not find user", userID)
+		return routing.NewHTTPError(400, "Invalid user.")
 	}
 
-	log.Println(user)
-
 	// auth with twitter
+	client, err = authTwitter(&environment.Twitter.Key, &environment.Twitter.Secret, &user.Twitter.Token, &user.Twitter.Secret)
+	if err != nil {
+		log.Println("could not authenticate", userID)
+		return routing.NewHTTPError(400, "Invalid user.")
+	}
 
 	return nil
+}
+
+func authTwitter(consumerKey, consumerSecret, accessToken, accessSecret *string) (*twitter.Client, error) {
+	config := oauth1.NewConfig(*consumerKey, *consumerSecret)
+	token := oauth1.NewToken(*accessToken, *accessSecret)
+	// OAuth1 http.Client will automatically authorize Requests
+	httpClient := config.Client(oauth1.NoContext, token)
+
+	// Twitter client
+	client := twitter.NewClient(httpClient)
+
+	return client, nil
 }
